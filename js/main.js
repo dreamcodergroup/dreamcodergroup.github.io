0829e18cb18f8c2f76ce95deae4cbf7f39026899
Jingru <niejingru@gmail.com>
2014-09-11 16:46:53 +0800

    Bugfixes.

--- js/main.js
+++ js/main.js
@@ -134,9 +134,11 @@ function scroll(id, delta)
 function scrollOn(x, speed)
 {
 	speed = speed - 15;
-	if (speed <= 10)
-		speed = 1;
 	var browserTop = document.body.scrollTop || document.documentElement.scrollTop;
+	if (speed <= 5)
+	{
+		speed = 5;
+	}
 	var dest = Math.ceil(Math.abs(x - browserTop) / speed);
 	if (x < browserTop)
 		dest = browserTop - dest;
